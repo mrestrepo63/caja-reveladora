231d2507ec0d9a52bfaeeb1b20d653778f6522f9
Mafe Restrepo <maferestrepo63@gmail.com>
2025-04-04 12:09:42 -0500

    Modificación ruta de acceso

--- segmentai.py
+++ segmentai.py
@@ -1,4 +1,67 @@
+# from segment import SamPredictor, sam_model_registry
+# sam = sam_model_registry["<model_type>"](checkpoint="<path/to/checkpoint>")
+# predictor = SamPredictor(sam)
+# predictor.set_image()
+# masks, _, _ = predictor.predict(<input_prompts>)
+
+import sys
+import os
+import matplotlib.pyplot as plt
+import torch 
+import numpy as np
+
+sys.path.append(os.path.abspath("segment-anything"))
+from segment_anything import sam_model_registry
+
+import sys
+sys.path.append("..")
+from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
+
+sam_checkpoint = r"C:\Users\HOME\OneDrive - Universidad CES\Desktop\HGM\caja reveladora\segment-anything\checkpoints\sam_vit_h_4b8939.pth"
+model_type = "default"
+
+device = "cpu"
+
+sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+sam.to(device=device)
+
+mask_generator = SamAutomaticMaskGenerator(sam)
+
+import cv2
+
+# Cargar la imagen
+image = cv2.imread(r"C:\Users\HOME\OneDrive - Universidad CES\Desktop\HGM\caja reveladora\src\IMG-20250310-WA0035.jpg")
+
+# Asegurar que la imagen se cargó correctamente
+if image is None:
+    raise ValueError("No se pudo cargar la imagen. Verifica la ruta.")
+
+def show_anns(anns):
+    if len(anns) == 0:
+        return
+    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
+    ax = plt.gca()
+    ax.set_autoscale_on(False)
+
+    img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 4))
+    img[:,:,3] = 0
+    for ann in sorted_anns:
+        m = ann['segmentation']
+        color_mask = np.concatenate([np.random.random(3), [0.35]])
+        img[m] = color_mask
+    ax.imshow(img)
+
 from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
 sam = sam_model_registry["default"](checkpoint=r"C:\Users\HOME\OneDrive - Universidad CES\Desktop\HGM\caja reveladora\segment-anything\checkpoints\sam_vit_h_4b8939.pth")
 mask_generator = SamAutomaticMaskGenerator(sam)
-masks = masks = mask_generator.generate(r"C:\Users\HOME\OneDrive - Universidad CES\Desktop\HGM\caja reveladora\src\IMG-20250310-WA0033.jpg")
+masks = mask_generator.generate(image)
+mask1=mask_generator.generate(image)
+
+print(len(masks))
+print(masks[0].keys())
+
+plt.figure(figsize=(20,20))
+plt.imshow(image)
+show_anns(masks)
+plt.axis('off')
+plt.show() 
